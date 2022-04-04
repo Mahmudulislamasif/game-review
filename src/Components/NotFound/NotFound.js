8b31f1c1f358de70fbbd212420b23b691a5ae440
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2022-04-04 21:24:05 +0600

    404 design add

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Emoji from '../../GameImage/angry-and-sad-emoji-115495132213jxf0zx4az.png'
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
+        <div className='style-error'>
+            <img src={Emoji} alt=''/>
             <h1>404</h1>
             <h1>This Page is not Available</h1>
         </div>
